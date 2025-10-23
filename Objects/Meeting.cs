dd0dc59fdecbf8db05f4f4978e0846f76d18f933
josefhardy <josefmhardy@gmail.com>
2025-10-23 13:51:22 +0100

    finished object creation

--- Objects/Meeting.cs
+++ Objects/Meeting.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 
-public class Class1
+public class Meeting
 {
-	public Class1()
-	{
-	}
+    public int meeting_id { get; set; }
+    public int student_id { get; set; }
+    public int supervisor_id { get; set; }
+    public DateTime meeting_date { get; set; }
+    public string notes { get; set; }
+
+    public Meeting(int meetingId, int studentId, int supervisorId, DateTime meetingDate, string notes)
+    {
+        meeting_id = meetingId;
+        student_id = studentId;
+        supervisor_id = supervisorId;
+        meeting_date = meetingDate;
+        this.notes = notes;
+    }
+    public Meeting() { }
 }
